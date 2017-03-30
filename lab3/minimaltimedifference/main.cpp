fae52f8bf94c55e4bdf525f420986f5fc980eeb5
BartoszG <bartosz-gaj@wp.pl>
2017-03-30 03:14:14 +0200

    Lab3 preparings to refactoring

--- lab3/minimaltimedifference/main.cpp
+++ lab3/minimaltimedifference/main.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 int main(){
     cout << minimaltimedifference::MinimalTimeDifference({"1:00", "23:59", "17:56", "14:13", "15:40"});
-   // cout << minimaltimedifference::ToMinutes("18:00");
 
 
     return 0;

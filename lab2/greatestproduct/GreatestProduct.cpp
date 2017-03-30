64ad034493a018f39306570544a8ce3f21a319cc
BartoszG <bartosz-gaj@wp.pl>
2017-03-30 02:11:49 +0200

    Refactred GreatestProduct

--- lab2/greatestproduct/GreatestProduct.cpp
+++ lab2/greatestproduct/GreatestProduct.cpp
@@ -1,32 +1,39 @@
 //
 // Created by ASUSTek on 2017-03-09.
 //
-
-
 #include "GreatestProduct.h"
-using namespace std;
 
-int GreatestProduct(const vector<int> &numbers, int k){
-    vector<int> liczby = numbers;
-    sort (liczby.begin(), liczby.end());
-    int wynik = 1;
-    int dodatnie = 1;
-    int ujemne = 0;
-    for (int i = 0; i < k/2 ; ++i) {
-        if(liczby[liczby.size()-dodatnie]*liczby[liczby.size()-dodatnie-1]>liczby[ujemne]*liczby[ujemne+1] or (liczby[liczby.size()-dodatnie]<0 and k%2!=0)){
-            wynik = wynik * liczby[liczby.size()-dodatnie]*liczby[liczby.size()-dodatnie-1];
-            dodatnie+=2; } else{
-            wynik = wynik * liczby[ujemne]*liczby[ujemne+1];
-            ujemne +=2;
+using std::vector;
+using std::sort;
+
+int GreatestProduct(const vector<int> &numbers, int k) {
+    vector<int> numbers_edited = numbers;
+    int result = 1;
+    int positive = 1;
+    int negative = 0;
+
+    sort(numbers_edited.begin(), numbers_edited.end());
+
+    for (int i = 0; i < k / 2; ++i) {
+        int first_positive = numbers_edited[numbers_edited.size() - positive - 1];
+        int second_positive = numbers_edited[numbers_edited.size() - positive];
+        int first_negative = numbers_edited[negative];
+        int second_negative = numbers_edited[negative + 1];
+
+        if (second_positive * first_positive > first_negative * second_negative or (second_positive < 0 and k % 2 != 0)) {
+            result = result * second_positive * first_positive;
+            positive += 2;
+        } else {
+            result = result * first_negative * second_negative;
+            negative += 2;
         }
     }
-    if(k%2!=0){
-        wynik = wynik*liczby[liczby.size()-dodatnie];
+    int second_positive = numbers_edited[numbers_edited.size() - positive];
+
+    if (k % 2 != 0) {
+        result = result * second_positive;
     }
-    return wynik;
-
-
-
+    return result;
 
 
 }

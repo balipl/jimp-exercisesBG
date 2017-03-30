41f58c42af6403519a802f2b73afbefdfd8bfc72
BartoszG <bartosz-gaj@wp.pl>
2017-03-30 10:00:17 +0200

    Refactored TinyUrl

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -6,47 +6,43 @@
 
 
 namespace tinyurl {
-    using namespace std;
+    using std::unique_ptr;
+    using std::make_unique;
 
-    std::unique_ptr<TinyUrlCodec> Init(){
+    std::unique_ptr<TinyUrlCodec> Init() {
         unique_ptr<TinyUrlCodec> p = make_unique<TinyUrlCodec>();
         return p;
     }
 
-    void NextHash(std::array<char, 6> *state){
-        string dict = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        string newurl;
+    void NextHash(std::array<char, 6> *state) {
+        string symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        string new_url;
         bool next_line = 1;
-        for (int i = 5; i >=0 ; --i) {
-
-            if ((*state)[i] == 'z' && next_line == 1)
-            {(*state)[i] = '0'; }
-            else if (next_line == 1)
-            {(*state)[i] = dict[dict.find(((*state)[i])) + 1];
-            next_line = 0;}
-
-            newurl = (*state)[i] + newurl;
+        for (int i = 5; i >= 0; --i) {
+            if ((*state)[i] == 'z' && next_line == 1) {
+                (*state)[i] = '0';
+            } else if (next_line == 1) {
+                (*state)[i] = symbols[symbols.find(((*state)[i])) + 1];
+                next_line = 0;
+            }
+            new_url = (*state)[i] + new_url;
         }
-
-
     }
-    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec){
-        string newurl;
 
-
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec) {
+        string new_url;
         for (int i = 0; i < 6; ++i) {
-            newurl += (*codec)->tab[i];
+            new_url += (*codec)->hash_tab[i];
         }
-        NextHash(&(*codec)->tab);
+        NextHash(&(*codec)->hash_tab);
 
-        (*codec)->mapa[newurl]=url;
-        return newurl;
+        (*codec)->mapa[new_url] = url;
+        return new_url;
     }
-    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash){
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash) {
         string url;
         url = codec->mapa[hash];
         return url;
     }
-
-
 }
--- lab3/tinyurl/TinyUrl.h
+++ lab3/tinyurl/TinyUrl.h
@@ -13,18 +13,20 @@
 #include <iostream>
 
 
-namespace tinyurl{
+namespace tinyurl {
     using ::std::string;
-    struct TinyUrlCodec{
+    struct TinyUrlCodec {
         std::map<string, string> mapa;
-        std::array<char ,6> tab;
+        std::array<char, 6> hash_tab;
     };
 
     std::unique_ptr<TinyUrlCodec> Init();
-    void NextHash(std::array<char, 6> *state);
-    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
-    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
 
+    void NextHash(std::array<char, 6> *state);
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
 }
 
 #endif //JIMP_EXERCISES_TINYURL_H

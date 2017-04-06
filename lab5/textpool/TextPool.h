5a507470ec3b63b6ffa4f64b8b4548e6d90b5389
BartoszG <bartosz-gaj@wp.pl>
2017-04-06 10:24:58 +0200

    fully operational textPool

--- lab5/textpool/TextPool.h
+++ lab5/textpool/TextPool.h
@@ -9,11 +9,31 @@
 #include <string>
 #include <experimental/string_view>
 #include <set>
+#include <iostream>
+#include <vector>
 
 
-using namespace pool{
+
+namespace pool{
+
 class TextPool {
+public:
+    TextPool();
+    ~TextPool();
 
+    TextPool(TextPool &&textpool); // konstruktor przenoszacy
+
+    TextPool &operator=(TextPool &&textpool); //operator kopiujacy
+
+    //konstruktor z listą initializacyjna
+    TextPool(const std::initializer_list<std::experimental::string_view> pool);
+
+    std::experimental::string_view Intern(const std::string &str);
+
+    size_t StoredStringCount() const;
+
+private:
+    std::set<std::experimental::string_view> pool_;
 };
 
 }

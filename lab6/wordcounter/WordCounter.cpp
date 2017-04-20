8c3ff8c3747a75f6a27057a91db37d49766a3b37
balipl <bartosz-gaj@wp.pl>
2017-04-20 12:42:13 +0200

    edited wordcounter. refactored and added frominputstream

--- lab6/wordcounter/WordCounter.cpp
+++ lab6/wordcounter/WordCounter.cpp
@@ -4,43 +4,66 @@
 
 #include "WordCounter.h"
 
-namespace datastructures{
+namespace datastructures {
     int WordCounter::DistinctWords() {
         int distinctwords = 0;
 
-        for(auto word : slownik_){
-            distinctwords+=1;
+        for (auto word : dictionary_) {
+            distinctwords += 1;
         }
         return distinctwords;
     }
 
     int WordCounter::TotalWords() {
         int totalwords = 0;
-        for(auto word : slownik_){
-            totalwords+= word.second.GetCounts();
+        for (auto word : dictionary_) {
+            totalwords += word.second.GetCounts();
         }
         return totalwords;
     }
 
     std::set<Word> WordCounter::Words() {
-        std::set<Word> wyjscie;
+        std::set<Word> exit_set;
 
-        for(auto v : slownik_){
-            wyjscie.insert(v.first);
+        for (auto v : dictionary_) {
+            exit_set.insert(v.first);
         }
-        return wyjscie;
+        return exit_set;
     }
 
     WordCounter::WordCounter(const std::initializer_list<Word> words) {
 
-        for(auto v : words){
-            if (slownik_.find(v) == slownik_.end())
-            {slownik_.emplace(v,1);}
-            else
-            {
-                slownik_[v].PlusCounts();
+        for (auto v : words) {
+            if (dictionary_.find(v) == dictionary_.end()) {
+                dictionary_.emplace(v, 1);
+            } else {
+                dictionary_[v].PlusCounts();
             }
         }
+    }
+
+    WordCounter WordCounter::FromInputStream(std::istream *input) {
+        WordCounter exit_word;
+        std::string word;
+        while (*input >> word) {
+            std::string checked_word = "";
+
+            for (int i = 0; i <word.length(); ++i) {
+                if((word[i]>= 65 and word[i]<=90) or (word[i]>=97 and word[i]<=122))
+                {
+                    checked_word += word[i];
+                }
+
+                Word new_word = Word(checked_word);
+                if (dictionary_.find(new_word) == dictionary_.end()) {
+                    dictionary_.emplace(new_word, 1);
+                } else {
+                    dictionary_[new_word].PlusCounts();
+                }
+            }
+        }
+
+
     }
 
     WordCounter::WordCounter() {} //konstruktor bezparametryczny
--- lab6/wordcounter/WordCounter.h
+++ lab6/wordcounter/WordCounter.h
@@ -11,17 +11,23 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <istream>
+
 namespace datastructures {
 
     class Word {
     private:
-    std::string word_;
+        std::string word_;
     public:
-        Word() ;
+        Word();
+
         Word(std::string);
+
         ~Word();
-        void SetWord(std::string word){word_ = word;};
-        std::string GetWord() const {return word_;};
+
+        void SetWord(std::string word) { word_ = word; };
+
+        std::string GetWord() const { return word_; };
     };
 
     class Counts {
@@ -29,53 +35,51 @@ namespace datastructures {
         int counter_;
     public:
         Counts();
+
         Counts(int);
+
         ~Counts();
 
-        int GetCounts() const {return counter_;};
-        void PlusCounts() { ++counter_;};
+        int GetCounts() const { return counter_; };
+
+        void PlusCounts() { ++counter_; };
     };
 
     class WordCounter {
     private:
-        std::map<Word,Counts> slownik_;
+        std::map<Word, Counts> dictionary_;
 
     public:
-        int DistinctWords();
-        int TotalWords();
-        std::set<Word> Words();
+        WordCounter();//konstruktor inicjalizujacy pusty slownik
 
         WordCounter(const std::initializer_list<Word> words);  //konstruktor z listą inicjalizacyjną pozwalający zliczyć podane słowa
-        WordCounter();//konstruktor inicjalizujacy pusty slownik
+
         ~WordCounter();
+
+        int DistinctWords();
+
+        int TotalWords();
+
+        std::set<Word> Words();
+
+        WordCounter FromInputStream(std::istream *input);
+
         int operator[](std::string index) {
             Word index1 = Word(index);
-
-            if (slownik_.find(index1) != slownik_.end()) {
-                return slownik_[index1].GetCounts();
-            } else {
-                return 0;
-            }
+            return dictionary_.find(index1) != dictionary_.end() ? dictionary_[index1].GetCounts() : 0 ;
         }
     };
 
-    inline bool operator< (const Word word1,const Word word2)
-    {
-        if(word1.GetWord()[0] < word2.GetWord()[0]) return true;
-        else
-        return false;
+    inline bool operator<(const Word word1, const Word word2) {
+        return word1.GetWord()[0] < word2.GetWord()[0] ? true : false;
     }
-    inline bool operator== (const Word word1,const Word word2)
-    {
-        if(word1.GetWord()[0] == word2.GetWord()[0]) return true;
-        else
-            return false;
+
+    inline bool operator==(const Word word1, const Word word2) {
+        return word1.GetWord()[0] == word2.GetWord()[0] ?true : false;
     }
-    inline bool operator== (const Counts count1, const Counts count2)
-    {
-        if(count1.GetCounts() == count2.GetCounts()) return true;
-        else
-            return false;
+
+    inline bool operator==(const Counts count1, const Counts count2) {
+        return count1.GetCounts() == count2.GetCounts()?true : false;
     }
 }
 #endif //JIMP_EXERCISES_WORDCOUNTER_H

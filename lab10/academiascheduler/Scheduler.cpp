55b27dc5f916f4f4023b49d459a7d6593449cfbf
balipl <bartosz-gaj@wp.pl>
2017-05-18 12:59:22 +0200

    Code refactored.

--- lab10/academiascheduler/Scheduler.cpp
+++ lab10/academiascheduler/Scheduler.cpp
@@ -5,15 +5,12 @@
 #include "Scheduler.h"
 
 
-namespace academia
-{
+namespace academia {
     Schedule Schedule::OfTeacher(int teacher_id) const {
         Schedule teacher_wektor;
 
-        for( auto n : scheduled_items_)
-        {
-            if(n.TeacherId() == teacher_id)
-            {
+        for (auto n : scheduled_items_) {
+            if (n.TeacherId() == teacher_id) {
                 teacher_wektor.InsertScheduleItem(n);
             }
         }
@@ -23,10 +20,8 @@ namespace academia
     Schedule Schedule::OfRoom(int room_id) const {
         Schedule room_wektor;
 
-        for(auto &n : scheduled_items_)
-        {
-            if(n.RoomId() == room_id)
-            {
+        for (auto &n : scheduled_items_) {
+            if (n.RoomId() == room_id) {
                 room_wektor.InsertScheduleItem(n);
             }
         }
@@ -37,10 +32,8 @@ namespace academia
     Schedule Schedule::OfYear(int year) const {
         Schedule year_wektor;
 
-        for(auto &n : scheduled_items_)
-        {
-            if(n.Year() == year)
-            {
+        for (auto &n : scheduled_items_) {
+            if (n.Year() == year) {
                 year_wektor.InsertScheduleItem(n);
             }
         }
@@ -49,19 +42,17 @@ namespace academia
     }
 
     std::vector<int> Schedule::AvailableTimeSlots(int n_time_slots) const {
-        bool is_available[n_time_slots+1];
+        bool is_available[n_time_slots + 1];
         std::vector<int> available_slots;
 
-        for (int i = 1; i < n_time_slots+1; ++i) {
-            is_available[i]= true;
+        for (int i = 1; i < n_time_slots + 1; ++i) {
+            is_available[i] = true;
         }
-        for(auto n : scheduled_items_)
-        {
+        for (auto n : scheduled_items_) {
             is_available[n.TimeSlot()] = false;
         }
-        for (int j = 1; j < n_time_slots+1; ++j) {
-            if(is_available[j])
-            {
+        for (int j = 1; j < n_time_slots + 1; ++j) {
+            if (is_available[j]) {
                 available_slots.emplace_back(j);
             }
         }
@@ -69,7 +60,7 @@ namespace academia
     }
 
     void Schedule::InsertScheduleItem(const SchedulingItem &item) {
-scheduled_items_.emplace_back(item);
+        scheduled_items_.emplace_back(item);
     }
 
     size_t Schedule::Size() const {
@@ -86,10 +77,10 @@ scheduled_items_.emplace_back(item);
 
     SchedulingItem::SchedulingItem(int course_id, int teacher_id, int room_id, int time_slot, int year) {
         course_id_ = course_id;
-        teacher_id_=teacher_id;
-        room_id_=room_id;
+        teacher_id_ = teacher_id;
+        room_id_ = room_id;
         time_slot_ = time_slot;
-        year_=year;
+        year_ = year;
     }
 
     int SchedulingItem::CourseId() const {
@@ -120,61 +111,55 @@ scheduled_items_.emplace_back(item);
 
         Schedule schedule;
         int actual_time_slot = 1;
-        bool sth = false;
         int actual_room = 0;
         int room_count = rooms.size();
         bool found_solution = false;
-        vector<int> used_years_an_hour[n_time_slots+1];
+        vector<int> used_years_an_hour[n_time_slots + 1];
 
 
-        for(auto &teacher_assignment : teacher_courses_assignment)
-        {
+        for (auto &teacher_assignment : teacher_courses_assignment) {
             int teacher_id = teacher_assignment.first;
             std::vector<int> teacher_courses = teacher_assignment.second;
 
-            for(auto &teacher_single_course : teacher_courses)
-            {
+            for (auto &teacher_single_course : teacher_courses) {
                 found_solution = false;
 
-                for(auto &year_assignment : courses_of_year)
-                {
+                for (auto &year_assignment : courses_of_year) {
                     int year_id = year_assignment.first;
                     std::set<int> year_courses = year_assignment.second;
 
-                    for(auto &year_single_course : year_courses)
-                    {
+                    for (auto &year_single_course : year_courses) {
 
-                        if(teacher_single_course == year_single_course and std::find(used_years_an_hour[actual_time_slot].begin(),used_years_an_hour[actual_time_slot].end(),year_id) ==  used_years_an_hour[actual_time_slot].end())
+                        if (teacher_single_course == year_single_course and
+                            std::find(used_years_an_hour[actual_time_slot].begin(),
+                                      used_years_an_hour[actual_time_slot].end(), year_id) ==
+                            used_years_an_hour[actual_time_slot].end())
                         {
                             found_solution = true;
-                            schedule.InsertScheduleItem(SchedulingItem(teacher_single_course,teacher_id,rooms[actual_room%room_count],actual_time_slot,year_id));
+                            schedule.InsertScheduleItem(
+                                    SchedulingItem(teacher_single_course, teacher_id, rooms[actual_room],
+                                                   actual_time_slot, year_id));
                             used_years_an_hour[actual_time_slot].emplace_back(year_id);
-                            if( actual_room == room_count)
-                            {
-                                throw NoViableSolutionFound("DHEHE");
+                            if (actual_room == room_count and actual_time_slot == n_time_slots-1) {
+                                throw NoViableSolutionFound("No Viable Solution Found");
                             }
 
-                            if(actual_time_slot < n_time_slots) {
+                            if (actual_time_slot < n_time_slots) {
                                 actual_time_slot++;
-                            }
-                            else if(actual_time_slot == n_time_slots){
-                                actual_time_slot =0;
+                            } else if (actual_time_slot == n_time_slots) {
+                                actual_time_slot = 0;
                                 actual_room++;
-
-
                             }
-                            //cout << "XXX: " << teacher_single_course << " " << teacher_id << " " << year_id << std::endl;
                         }
                     }
                 }
-                if(!found_solution) {throw NoViableSolutionFound("XXX");}
+                if (!found_solution) { throw NoViableSolutionFound("No Viable Solution Found"); }
             }
         }
 
 
         return schedule;
     }
-
 
 
 }
